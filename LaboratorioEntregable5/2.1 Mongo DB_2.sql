da705087c6412f2e94b72763df3bcba223a030d6
Daillent <daillent.conrado.ttc.071@unilibre.edu.co>
2024-12-12 22:56:40 -0500

    Add files via upload

---
+++ LaboratorioEntregable5/2.1 Mongo DB_2.sql
@@ -0,0 +1,15 @@
+// Conectar y crear la colección 'Estudiantes'
+db.Estudiantes.insertMany([
+    { "nombre": "Juana", "edad": 20, "ciudad": "Bogotá" },
+    { "nombre": "Manuela", "edad": 22, "ciudad": "Medellín" },
+    { "nombre": "Luisa", "edad": 19, "ciudad": "Cali" }
+]);
+
+// Consultar todos los documentos de la colección
+db.Estudiantes.find();
+
+// Filtrar estudiantes por ciudad
+db.Estudiantes.find({ "ciudad": "Medellín" });
+
+// Consultar estudiantes mayores de 20 años
+db.Estudiantes.find({ "edad": { $gt: 20 } });
